e2a846a3a9aa109ef18898f3acc63b8bce2871c8
luyi <luyia@trip.com>
2023-09-13 19:47:45 +0800

    modify prettier

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -1,43 +1,41 @@
 export const iPhoneModels = {
     iPhone15Pro: [
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "128GB", model: "MTQ43CH/A", },
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "256GB", model: "MTQ83CH/A", },
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "512GB", model: "MTQD3CH/A", },
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "1TB", model: "MTQH3CH/A", },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '128GB', model: 'MTQ43CH/A' },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '256GB', model: 'MTQ83CH/A' },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '512GB', model: 'MTQD3CH/A' },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '1TB', model: 'MTQH3CH/A' },
 
-        { color: { value: "white", text: '白色钛金属', }, capacity: "128GB", model: "MTQ53CH/A", },
-        { color: { value: "white", text: '白色钛金属', }, capacity: "256GB", model: "MTQ93CH/A", },
-        { color: { value: "white", text: '白色钛金属', }, capacity: "512GB", model: "MTQE3CH/A", },
-        { color: { value: "white", text: '白色钛金属', }, capacity: "1TB", model: "MTQJ3CH/A", },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '128GB', model: 'MTQ53CH/A' },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '256GB', model: 'MTQ93CH/A' },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '512GB', model: 'MTQE3CH/A' },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '1TB', model: 'MTQJ3CH/A' },
 
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "128GB", model: "MTQ73CH/A", },
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "256GB", model: "MTQC3CH/A", },
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "512GB", model: "MTQG3CH/A", },
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "1TB", model: "MTQL3CH/A", },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '128GB', model: 'MTQ73CH/A' },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '256GB', model: 'MTQC3CH/A' },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '512GB', model: 'MTQG3CH/A' },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '1TB', model: 'MTQL3CH/A' },
 
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "128GB", model: "MTQ63CH/A", },
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "256GB", model: "MTQA3CH/A", },
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "512GB", model: "MTQF3CH/A", },
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "1TB", model: "MTQK3CH/A", },
-
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '128GB', model: 'MTQ63CH/A' },
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '256GB', model: 'MTQA3CH/A' },
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '512GB', model: 'MTQF3CH/A' },
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '1TB', model: 'MTQK3CH/A' },
     ],
 
     iPhone15ProMax: [
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "256GB", model: "MU2N3CH/A", },
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "512GB", model: "MU2T3CH/A", },
-        { color: { value: "black", text: '黑色钛金属', }, capacity: "1TB", model: "MU2X3CH/A", },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '256GB', model: 'MU2N3CH/A' },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '512GB', model: 'MU2T3CH/A' },
+        { color: { value: 'black', text: '黑色钛金属' }, capacity: '1TB', model: 'MU2X3CH/A' },
 
-        { color: { value: "white", text: '白色钛金属', }, capacity: "256GB", model: "MU2P3CH/A", },
-        { color: { value: "white", text: '白色钛金属', }, capacity: "512GB", model: "MU2U3CH/A", },
-        { color: { value: "white", text: '白色钛金属', }, capacity: "1TB", model: "MU2Y3CH/A", },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '256GB', model: 'MU2P3CH/A' },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '512GB', model: 'MU2U3CH/A' },
+        { color: { value: 'white', text: '白色钛金属' }, capacity: '1TB', model: 'MU2Y3CH/A' },
 
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "256GB", model: "MU2R3CH/A", },
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "512GB", model: "MU2W3CH/A", },
-        { color: { value: "blue", text: '蓝色钛金属', }, capacity: "1TB", model: "MU613CH/A", },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '256GB', model: 'MU2R3CH/A' },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '512GB', model: 'MU2W3CH/A' },
+        { color: { value: 'blue', text: '蓝色钛金属' }, capacity: '1TB', model: 'MU613CH/A' },
 
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "256GB", model: "MU2Q3CH/A", },
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "512GB", model: "MU2V3CH/A", },
-        { color: { value: "primary", text: '原色钛金属', }, capacity: "1TB", model: "MU603CH/A", },
-
-    ]
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '256GB', model: 'MU2Q3CH/A' },
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '512GB', model: 'MU2V3CH/A' },
+        { color: { value: 'primary', text: '原色钛金属' }, capacity: '1TB', model: 'MU603CH/A' },
+    ],
 }
