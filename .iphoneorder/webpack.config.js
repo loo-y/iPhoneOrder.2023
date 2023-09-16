96fd7e551e72e66feeb6ed32a4f5cd0637b08919
Loo <loo@Mac-mini-M2.lan>
2023-09-16 15:16:46 +0800

    remove dist

--- .iphoneorder/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-    entry: './dist/index',
-    output: {
-        path: path.resolve(__dirname, 'dist/publish'),
-        filename: 'iPhoneOrder.js',
-    },
-}
